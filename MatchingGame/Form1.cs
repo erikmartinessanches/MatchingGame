ad99fd85a0d1f20db1c89d65c36b214c41ad3897
Erik Martines Sanches <erik.martinessanches@gmail.com>
2016-06-20 20:50:00 +0200

    Made use of a timer to hide the Labels.

--- MatchingGame/Form1.cs
+++ MatchingGame/Form1.cs
@@ -14,6 +14,8 @@ namespace MatchingGame
     {
         Random random = new Random();
         List<string> icons = new List<string>() { "!", "!", "N", "N", ",", ",", "k", "k", "b", "b", "v", "v", "w", "w", "z", "z" };
+        Label firstClicked = null;
+        Label secondClicked = null;
         
         public Form1()
         {
@@ -40,8 +42,20 @@ namespace MatchingGame
             }
         }
         /*Because you hooked up different label control Click event to a single event handler method, the same method is called no matter which label the user chooses. The event handler method needs to know which label was chosen, so it uses the name sender to identify the label control. The first line of the method tells the program that it's not just a generic object, but specifically a label control, and that it uses the name clickedLabel to access the label's properties and methods.*/
+        /// <summary>
+        /// Every label's Click event is handled by this event handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void label_Click(object sender, EventArgs e)
         {
+            // The timer is only on after two non-matching 
+            // icons have been shown to the player, 
+            // so ignore any clicks if the timer is running.
+            if (timer1.Enabled == true)
+            {
+                return;
+            }
             Label clickedLabel = sender as Label;
             if (clickedLabel != null)
             {
@@ -49,8 +63,38 @@ namespace MatchingGame
                 {
                     return;
                 }
-                clickedLabel.ForeColor = Color.Black;
+                /*If firstClicked is null, this is the first icon in the pair that the player clicked, so set firstClicked to the label that the player clicked, change its color to black, and return. */
+                if (firstClicked == null)
+                {
+                    firstClicked = clickedLabel;
+                    firstClicked.ForeColor = Color.Black;
+                    return;
+                }
+                // If the player gets this far, the timer isn't
+                // running and firstClicked isn't null,
+                // so this must be the second icon the player clicked.
+                // Set its color to black.
+                secondClicked = clickedLabel;
+                secondClicked.ForeColor = Color.Black;
+                // If the player gets this far, the player 
+                // clicked two different icons, so start the 
+                // timer (which will wait three quarters of 
+                // a second, and then hide the icons).
+                timer1.Start();
             }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            // Hide both icons
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+            // Reset firstClicked and secondClicked 
+            // so the next time a label is
+            // clicked, the program knows it's the first click
+            firstClicked = null;
+            secondClicked = null;
         }
     }
 }
