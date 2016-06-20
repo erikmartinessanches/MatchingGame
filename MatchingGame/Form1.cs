5c5037e29846198ea207ec632c74c4c7536e7a07
Erik Martines Sanches <erik.martinessanches@gmail.com>
2016-06-20 19:42:15 +0200

    Inserted an icon into each label randomly and made them invisible.

--- MatchingGame/Form1.cs
+++ MatchingGame/Form1.cs
@@ -12,9 +12,35 @@ namespace MatchingGame
 {
     public partial class Form1 : Form
     {
+        Random random = new Random();
+        List<string> icons = new List<string>() { "!", "!", "N", "N", ",", ",", "k", "k", "b", "b", "v", "v", "w", "w", "z", "z" };
+        
         public Form1()
         {
             InitializeComponent();
+            AssignIconsToSquares();
+        }
+        
+        /// <summary>
+        /// Assign each icon from the list of icons to a random square.
+        /// </summary>
+        private void AssignIconsToSquares()
+        {
+            // The TableLayoutPanel has 16 labels,
+            // and the icon list has 16 icons,
+            // so an icon is pulled at random from the list
+            // and added to each label.
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if(iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    iconLabel.Visible = false;
+                    icons.RemoveAt(randomNumber);
+                }
+            }
         }
     }
 }
