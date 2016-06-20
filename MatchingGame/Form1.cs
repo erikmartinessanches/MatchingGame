548d639f507a7fcc094578cf864bae6568153052
Erik Martines Sanches <erik.martinessanches@gmail.com>
2016-06-20 20:12:27 +0200

    Event handler to make label controls visible when clicked.

--- MatchingGame/Form1.cs
+++ MatchingGame/Form1.cs
@@ -26,10 +26,7 @@ namespace MatchingGame
         /// </summary>
         private void AssignIconsToSquares()
         {
-            // The TableLayoutPanel has 16 labels,
-            // and the icon list has 16 icons,
-            // so an icon is pulled at random from the list
-            // and added to each label.
+            /* The TableLayoutPanel has 16 labels, and the icon list has 16 icons, so an icon is pulled at random from the list and added to each label.*/
             foreach (Control control in tableLayoutPanel1.Controls)
             {
                 Label iconLabel = control as Label;
@@ -37,9 +34,22 @@ namespace MatchingGame
                 {
                     int randomNumber = random.Next(icons.Count);
                     iconLabel.Text = icons[randomNumber];
-                    iconLabel.Visible = false;
+                    iconLabel.ForeColor = iconLabel.BackColor;
                     icons.RemoveAt(randomNumber);
                 }
+            }
+        }
+        /*Because you hooked up different label control Click event to a single event handler method, the same method is called no matter which label the user chooses. The event handler method needs to know which label was chosen, so it uses the name sender to identify the label control. The first line of the method tells the program that it's not just a generic object, but specifically a label control, and that it uses the name clickedLabel to access the label's properties and methods.*/
+        private void label_Click(object sender, EventArgs e)
+        {
+            Label clickedLabel = sender as Label;
+            if (clickedLabel != null)
+            {
+                if (clickedLabel.ForeColor == Color.Black)
+                {
+                    return;
+                }
+                clickedLabel.ForeColor = Color.Black;
             }
         }
     }
